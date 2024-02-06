f17dfa5b81c59517076aac6c7509b2adfcb48536
stanislas1200 <34397853+stanislas1200@users.noreply.github.com>
2024-02-06 18:59:14 +0100

    -http request test GET -config import test -index.html test

--- include/Webserv.hpp
+++ include/Webserv.hpp
@@ -5,6 +5,10 @@
 # include <sys/socket.h>
 # include <netinet/in.h>
 # include <unistd.h>
+# include <errno.h>
+# include <string.h>
+# include <fstream>
+# include <stdlib.h>
 
 # define RED "\x1b[1m\x1b[38;2;255;0;0m"
 # define MB "\x1b[1m\x1b[38;2;25;25;599m"
@@ -12,3 +16,7 @@
 # define YELLOW "\x1b[1m\x1b[38;2;205;205;0m"
 # define DV "\x1b[1m\x1b[38;2;148;0;211m"
 # define C "\x1b[0m"
+
+typedef struct s_config {
+	int port;
+} t_config;
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,111 @@
 #include "../include/Webserv.hpp"
 
-void error(std::string str) {
-	std::cout << RED "Error: " YELLOW << str << std::endl; 
-	exit(1);
+void error(const char *type, const char *msg, const char *bold) {
+	std::cerr << RED "Error: " YELLOW << type << " " C << msg;
+	if (bold)
+		std::cerr << DV " " << bold << C;
+	std::cerr << C << std::endl;
+}
+
+void getConfig(s_config *config, std::string file) {
+	std::ifstream confFILE(file.c_str());
+	if (!confFILE.is_open()) {
+		error("Config:", strerror(errno), NULL);
+		return ;
+	}
+	std::string line;
+	while (std::getline(confFILE, line)) {
+		std::cout << line << std::endl;
+		if (line.find("listen") != std::string::npos) {
+			config->port = atoi(line.substr(line.find(" ") + 1).c_str());
+			if (config->port < 0 || config->port > 65535)
+				error("Port:", "Invalid port number", NULL);
+		}
+	}
+}
+
+void handleGetRequest(int connection, std::string request) {
+	char filePath[1024];
+	sscanf(request.c_str(), "GET %255s", filePath);
+	std::string path = filePath;
+	if (path == "/") {
+		path = "/index.html";
+	}
+	std::string fullPath = "src/pages" + path;
+	std::cout << "Full path: " << fullPath << std::endl;
+	std::ifstream file(fullPath.c_str(), std::ios::binary);
+	if (!file.is_open()) {
+		error("File:", strerror(errno), NULL);
+		std::string response = "HTTP/1.0 404 Not Found\r\nContent-type:text/html\r\n\r\n";
+		std::string page = "<html>\n<head>\n<title>404 Not Found</title>\n</head>\n<body>\n<h2>404 Not Found</h2>\n</body>\n</html>\n\n";
+		send(connection, response.c_str(), response.length(), 0);
+		send(connection, page.c_str(), page.length(), 0);
+		return ;
+	}
+
+	send(connection, "HTTP/1.1 200 OK\r\n", strlen("HTTP/1.1 200 OK\r\n"), 0);
+    send(connection, "Content-Type: text/html\r\n\r\n", strlen("Content-Type: text/html\r\n\r\n"), 0);
+
+    // Send file content
+    char buffer[1024];
+    while (file.read(buffer, sizeof(buffer)).gcount() > 0) {
+		std::cout << "Sending file" << std::endl;
+		std::cout << file.gcount() << std::endl;
+        send(connection, buffer, file.gcount(), 0);
+    }
+
+    file.close();
+}
+
+void parseRequest(int connection, char *buffer) {
+	std::string request(buffer);
+	std::string method = request.substr(0, request.find(" "));
+
+	std::cout << "Method: " << method << std::endl;
+	if (method == "GET")
+		handleGetRequest(connection, request);
+}
+
+void handleConnection(int connection) {
+	
+		char buffer[1024];
+		read(connection, buffer, sizeof(buffer) - 1);
+
+		parseRequest(connection, buffer);
+
+		// std::string response = "HTTP/1.0 200 OK\r\nContent-type:text/html\r\n\r\n";
+		// std::string page = "<html>\n<head>\n<title>Hello World - First CGI Program</title>\n</head>\n<body>\n<h2>Hello World! This is my first CGI program</h2>\n</body>\n</html>\n\n";
+
+		// send(connection, response.c_str(), response.length(), 0);
+		// send(connection, page.c_str(), page.length(), 0);
+		// std::cout << buffer << std::endl;
 }
 
 int main(int ac, char **av) {
+	s_config config;
+
+	if (ac != 2) {
+		error("Usage:", av[0], "<config_file>");
+		std::cout << GREEN "WebServ: " << MB "Using default config file" C << std::endl;
+		getConfig(&config, "configs/default");
+	}
+	else
+		getConfig(&config, av[1]);
+
 	int socketFd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socketFd == -1)
-		error("socket");
+		error("Socket:", strerror(errno), NULL);
 	
 
 	sockaddr_in sockaddr;
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_addr.s_addr = INADDR_ANY;
-	sockaddr.sin_port = htons(443); // conf and multiple 
+	sockaddr.sin_port = htons(config.port);
 	if (bind(socketFd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) == -1)
-		error("bind");
+		error("Bind:", strerror(errno), NULL);
 
 	if (listen(socketFd, 10) == -1)
-		error("listen");
+		error("Listen:", strerror(errno), NULL);
 
 	while (1)
 	{
@@ -27,21 +113,10 @@ int main(int ac, char **av) {
 		int addrlen = sizeof(sockaddr);
 		int connection = accept(socketFd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
 		if (connection < 0)
-			error("connection");
+			error("Connection:", strerror(errno), NULL);
 
-		char buffer[10000];
-		read(connection, buffer, 10000);
-
-		std::string response = "HTTP/1.0 200 OK\r\nContent-type:text/html\r\n\r\n";
-		std::string page = "<html>\n<head>\n<title>Hello World - First CGI Program</title>\n</head>\n<body>\n<h2>Hello World! This is my first CGI program</h2>\n</body>\n</html>\n\n";
-
-		send(connection, response.c_str(), response.length(), 0);
-		send(connection, page.c_str(), page.length(), 0);
-		std::cout << buffer << std::endl;
+		handleConnection(connection);
 
 		close(connection);
 	}
-
-	(void)av;
-	(void)ac;
 }
