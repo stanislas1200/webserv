1e6abd936bf552f617bc3ca1859b2241a88c0a05
stanislas1200 <34397853+stanislas1200@users.noreply.github.com>
2024-04-17 11:21:23 +0200

    protect delete of uplaod folder

--- src/request/delete.cpp
+++ src/request/delete.cpp
@@ -1,6 +1,10 @@
 #include "../../include/request.hpp"
 
 int handleDeleteRequest(int connection, s_request request) { // TODO : replace space in filename
+	if (request.path == "/") {
+		send(connection, "HTTP/1.1 400 Bad Request\r\n", strlen("HTTP/1.1 400 Bad Request\r\n"), 0);
+		return error("DELETE:", "no file specified", NULL), 1;
+	}
 	if (std::remove(("upload/" + request.path).c_str()))
 	{
 		send(connection, "HTTP/1.1 404 Not Found\r\n", strlen("HTTP/1.1 404 Not Found\r\n"), 0);
