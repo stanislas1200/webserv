71a1302a4aee61d5b151768bda734a26f8130e23
Guillaume Duchesne <gduchesn@shi-r4-p2.s19.be>
2024-04-26 13:40:22 +0200

    cleaning autoindex

--- include/Webserv.hpp
+++ include/Webserv.hpp
@@ -17,6 +17,9 @@
 # include <iostream>
 # include <fcntl.h>
 # include <signal.h>
+#include <netinet/in.h>
+#include <dirent.h>
+#include <sys/stat.h>
 # include "ServConfig.hpp"
 # include "Location.hpp"
 
--- src/request/request.cpp
+++ src/request/request.cpp
@@ -182,32 +182,25 @@ std::string	replaceHexAndAmp(std::string src)
 	return (str);
 }
 
-#include <netinet/in.h>
-#include <dirent.h>
-#include <sys/stat.h>
 void	autoindex(s_request *request, std::string path) {
 	std::stringstream ss;
 	std::string response;
 	DIR* dir;
     struct dirent* entry;
-	// std::string path = "./src/pages/";
 	path += request->path;
     ss << "<html><head><title>Index of " << path << "</title></head><body>";
     ss << "<h1>Index of " << path << "</h1><hr><pre>";
     ss << "</pre><hr></body></html>";
-    std::cout << "path:" << path.c_str() << std::endl;
     if ((dir = opendir(path.c_str())) != nullptr) {
         while ((entry = readdir(dir)) != nullptr) {
-            // if (entry->d_name[0] != '.') {
-                std::string fullPath = path + "/" + entry->d_name;
-                struct stat statbuf;
-                
-                if (stat(fullPath.c_str(), &statbuf) != -1 && S_ISDIR(statbuf.st_mode)) {
-                    ss << "<a href='" << entry->d_name << "/'>" << entry->d_name << "/</a><br>";
-                } else {
-                    ss << "<a href='" << entry->d_name << "'>" << entry->d_name << "</a><br>";
-                }
-            // }
+			std::string fullPath = path + "/" + entry->d_name;
+			struct stat statbuf;
+			
+			if (stat(fullPath.c_str(), &statbuf) != -1 && S_ISDIR(statbuf.st_mode)) {
+				ss << "<a href='" << entry->d_name << "/'>" << entry->d_name << "/</a><br>";
+			} else {
+				ss << "<a href='" << entry->d_name << "'>" << entry->d_name << "</a><br>";
+			}
         }
         
         closedir(dir);
@@ -297,10 +290,8 @@ int parseRequest(std::string header, s_request *request) {
 		}
 	}
 	if (request->path[request->path.size() - 1] == '/') {
-		std::cout << "UI\n";
 		if (request->conf.getAutoindex())
 			return (autoindex(request, request->conf.getRoot()), 1);
-		std::cout << "OUT\n";
 	}
 	sendError(404, *request);
 	return 1;
